e6a67478b1bf985866acc39893c8df6063fccb1d
ybadmus <yusif.badmus@gmail.com>
2021-03-19 07:05:07 +0800

    include session controller and routes to application

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
+  root "user#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users 
-
-  root "user#show"
 end
