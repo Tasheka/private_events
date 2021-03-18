2c2a39f6aeeb6735a2548c6aec4e7897d4343523
Tasheka Hamilton <tasheka.tech@gmail.com>
2021-03-18 18:06:43 -0400

    Configure project, add user controller and model

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+
+    def index
+      @users = User.all
+    end
+  
+    def new
+      @user = User.new
+    end
+  
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        session[:user_id] = @user.id
+        redirect_to root_path
+      else
+        render 'new'
+      end
+    end
+  
+    def show
+      @upcoming_events = current_user.events.upcoming_events
+      @past_events = current_user.events.past_events
+    end
+  end
+  
