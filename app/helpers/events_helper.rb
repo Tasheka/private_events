73f5d53cb085547d2266f0f4767086dc350ace4b
ybadmus <yusif.badmus@gmail.com>
2021-03-19 22:56:06 +0800

    fix a bug with the edit form for unauthorized updates

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,8 @@
 module EventsHelper
+  def same_as_event_creator
+    puts cookies[:user_id]
+    puts @event.user_id
+    puts cookies[:user_id].to_i.eql?(@event.user_id.to_i)
+    cookies[:user_id].to_i.eql?(@event.user_id.to_i) ? true : false
+  end
 end
