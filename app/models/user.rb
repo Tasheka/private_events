e45940a2532c3aa5ec4d77da91b0f0208a6b5dba
ybadmus <yusif.badmus@gmail.com>
2021-03-30 19:54:43 +0800

    fix a bug with user not saving

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :name, presence: true, uniqueness: true, length: { in: 3..12 }
+  validates :username, presence: true, uniqueness: true, length: { in: 3..12 }
   validates :email, presence: true, uniqueness: true
   has_many :events
 end
